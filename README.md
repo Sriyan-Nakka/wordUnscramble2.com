ea9e428678bca75b5bdd71d180e6a1a49b1a2800
Sriyan-Nakka <sriyannakka@gmail.com>
2025-08-10 18:57:54 +0530

    version 0.3

--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 Welcome to Word Unscramble 2! This is remake of my previous Word
 Unscramble game.
 
-The rules are simple. You have to unscramble the given word. You have
-5 lives and you have to unscramble a total of 5 scrambled words. If
-you lose all your lives, you lost. Good luck!
+The rules are simple. You have to unscramble the given word. You have set how many lives you have and you have to unscramble a total of 5 scrambled words. Maximum lives you can set is 15.If
+you lose all your lives, you lose. Good luck!
--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 const gameContainer = document.querySelector("#gameContainer");
+const wordPara = document.querySelector("#scrambledWordPara");
+
+let correctWord = "";
+let lives = 0;
+
+function startGame() {
+  while (lives <= 0 || lives >= 16 || isNaN(lives)) {
+    lives = Number(
+      prompt("Set how many lives you want for this current game?")
+    );
+  }
+
+  playTurn();
+}
+
+function playTurn() {
+  gameContainer.style.display = "block";
+
+  fetch("https://random-word-api.vercel.app/api?words=1")
+    .then((res) => res.json())
+    .then((data) => {
+      correctWord = data[0];
+      console.log(correctWord);
+      let wordSplit = correctWord.split("");
+
+      //word shuffler
+      for (let i = 0; i < 100; i++) {
+        let a = Math.floor(Math.random() * wordSplit.length);
+        let b = Math.floor(Math.random() * wordSplit.length);
+
+        let temp = wordSplit[a];
+        wordSplit[a] = wordSplit[b];
+        wordSplit[b] = temp;
+      }
+
+      wordPara.textContent = wordSplit.join("");
+    });
+}
