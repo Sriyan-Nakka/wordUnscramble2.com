e752aca43f504edcee109aad86c1667b5bc15636
Sriyan-Nakka <sriyannakka@gmail.com>
2025-08-11 23:15:03 +0530

    version 0.4

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const playButton = document.querySelector("#playButton");
 const gameContainer = document.querySelector("#gameContainer");
 const wordPara = document.querySelector("#scrambledWordPara");
+const guessForm = document.querySelector("#guessForm");
+const guessWord = document.querySelector("#guessWord");
 
 let correctWord = "";
 let lives = 0;
 
 function startGame() {
+  guessWord.value = "";
+  playButton.style.display = "none";
+
+  lives = 0;
+
   while (lives <= 0 || lives >= 16 || isNaN(lives)) {
     lives = Number(
       prompt("Set how many lives you want for this current game?")
     );
   }
 
+  displayLives();
   playTurn();
 }
 
@@ -36,4 +45,38 @@ function playTurn() {
 
       wordPara.textContent = wordSplit.join("");
     });
+}
+
+guessForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let guessedWord = guessWord.value;
+  if (guessedWord === correctWord) {
+    //correct function
+  } else {
+    lives--;
+    alert("You lost a Life!");
+    displayLives();
+  }
+});
+
+function displayLives() {
+  document.querySelector("#livesSpan").textContent = lives;
+  if (lives > 5) {
+    document.querySelector("#livesSpan").style.color = "var(--good-life)";
+  } else if (lives >= 3) {
+    document.querySelector("#livesSpan").style.color = "var(--close-life)";
+  } else if (lives <= 2 && lives > 0) {
+    document.querySelector("#livesSpan").style.color = "var(--bad-life)";
+  } else if (lives === 0) {
+    guessWord.disabled = true;
+    document.querySelector("#guessButton").disabled = true;
+    setTimeout(() => {
+      //loss function
+      guessWord.disabled = false;
+      guessWord.value = "";
+      document.querySelector("#guessButton").disabled = false;
+      alert("You lost! Click Play to play again.");
+      //todo: make it so when exited alert, it closes game container and shows play button
+    }, 1500);
+  }
 }
